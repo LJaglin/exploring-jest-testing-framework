32efa23729e9a955a76436d8e499a56533138cc3
LJaglin <ljaglinski@o2.pl>
2019-04-25 10:59:11 +0200

    Add test case (two negatives numbers) to test that checks sum of two numbers

--- app.test.js
+++ app.test.js
@@ -8,4 +8,5 @@ test('Should add two numbers', () => {
     expect(funToTest.addTwoNum(3, 3)).toBe(6);
     expect(funToTest.addTwoNum(3, 3)).not.toBe(5);
     expect(funToTest.addTwoNum(-2, 2)).toBe(0);
+    expect(funToTest.addTwoNum(-2, -2)).toBe(-4);
 });
