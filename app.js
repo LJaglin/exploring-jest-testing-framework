333039317b3cee8b7974b69cfe69a2c92a17ddcb
LJaglin <ljaglinski@o2.pl>
2019-04-25 11:28:53 +0200

    Fix function addTwoNum

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const funToTest = {
     returnHello: () => 'Hello',
-    addTwoNum: (num1, num2) => { return num1 + num2 }
+    addTwoNum: (num1, num2) => {  
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+            return null;
+        }
+        return num1 + num2;
+    }
 }   
 
 module.exports = funToTest;
