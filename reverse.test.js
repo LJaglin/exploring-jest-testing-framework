8c2c864517828fbb1b49de8e9300c75b9a7c54d2
LJaglin <ljaglinski@o2.pl>
2019-04-26 13:21:29 +0200

    Create test that check if function reverse a string

--- reverse.test.js
+++ reverse.test.js
@@ -3,3 +3,7 @@ const reverse = require('./reverse');
 test('reverseStr function exists', () => {
     expect(reverse).toBeDefined();
 });
+
+test('String reverses', () => {
+    expect(reverse('start')).toEqual('trats');
+});
