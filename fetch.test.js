53278e535d4cf493cf71f603ec983b95e843b655
LJaglin <ljaglinski@o2.pl>
2019-04-26 11:49:35 +0200

    Create test for fetching data using Async Await

--- fetch.test.js
+++ fetch.test.js
@@ -2,9 +2,16 @@
 const functions = require('./fetch');
 
 //Promise
-test('User fetched name should be Ervin Howell', () => {
+test('Promise - User fetched name should be Ervin Howell', () => {
     expect.assertions(1);
     return functions.fetchUser().then(data => {
         expect(data.name).toEqual('Ervin Howell');
     });
+});
+
+//Async Await
+test('Async Await - User fetched name should be Ervin Howell', async () => {
+    expect.assertions(1);
+    const fetchedData = await functions.fetchUser();
+    expect(fetchedData.name).toEqual('Ervin Howell');
 });
